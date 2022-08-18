a8dec580b9552ce5451c72cb0a28665c88ac2d0b
Grace Wang <t4_23@yahoo.com.tw>
2022-08-18 23:31:42 +0800

    Update 2022-08-18T15:31:37.266Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,25 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+// Initialize Swiper
+var swiper = new Swiper('.mySwiper', {
+  cssMode: true,
+  //loop: true,
+  autoplay: {
+    delay: 1000
+  },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  mousewheel: true,
+  keyboard: true
 });
 //# sourceMappingURL=all.js.map
