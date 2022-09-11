462d201fee9ded04007f1e45191df6272c7d258b
Grace Wang <t4_23@yahoo.com.tw>
2022-09-12 01:05:54 +0800

    add index.html and course.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,18 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// Initialize Swiper
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
